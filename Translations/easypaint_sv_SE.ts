27e456e35bfdf1a5fd3d905a646f1303499adb42
Tae-Woong Se <seotaewong40@gmail.com>
2018-01-05 23:42:35 +0900

    Added EasyPaint translations.

--- Translations/easypaint_sv_SE.ts
+++ Translations/easypaint_sv_SE.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+﻿<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="sv_SE">
 <context>
@@ -31,6 +31,7 @@
     <name>ImageArea</name>
     <message>
         <location filename="../imagearea.cpp" line="178"/>
+        <location filename="../imagearea.cpp" line="107"/>
         <source>Open image...</source>
         <translation>Öppna bild...</translation>
     </message>
@@ -58,6 +59,8 @@
     </message>
     <message>
         <location filename="../imagearea.cpp" line="242"/>
+
+        <location filename="../imagearea.cpp" line="155"/>
         <source>Untitled image</source>
         <translation>Namnlös bild</translation>
     </message>
@@ -65,6 +68,16 @@
         <location filename="../imagearea.cpp" line="244"/>
         <source>Save image...</source>
         <translation>Lagra bild...</translation>
+    </message>
+
+        <location filename="../imagearea.cpp" line="157"/>
+        <source>Save image...</source>
+        <translation>Lagra bild...</translation>
+    </message>
+    <message>
+        <location filename="../imagearea.cpp" line="158"/>
+        <source>*.png;;*.jpg;;*.jpeg;;*.bmp;;*.xbm;;*.xpm</source>
+        <translation>*.png;;*.jpg;;*.jpeg;;*.bmp;;*.xbm;;*.xpm</translation>
     </message>
 </context>
 <context>
@@ -74,81 +87,114 @@
         <location filename="../mainwindow.cpp" line="489"/>
         <location filename="../mainwindow.cpp" line="537"/>
         <location filename="../mainwindow.cpp" line="544"/>
+
+        <location filename="../mainwindow.cpp" line="83"/>
+        <location filename="../mainwindow.cpp" line="398"/>
         <source>Untitled Image</source>
         <translation>Namnlös bild</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="147"/>
+
+        <location filename="../mainwindow.cpp" line="124"/>
         <source>&amp;File</source>
         <translation>&amp;Arkiv</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="149"/>
+
+        <location filename="../mainwindow.cpp" line="126"/>
         <source>&amp;New</source>
         <translation>&amp;Ny</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="155"/>
+
+        <location filename="../mainwindow.cpp" line="134"/>
         <source>&amp;Open</source>
         <translation>&amp;Öppna</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="161"/>
+
+        <location filename="../mainwindow.cpp" line="142"/>
         <source>&amp;Save</source>
         <translation>&amp;Lagra</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="167"/>
+
+        <location filename="../mainwindow.cpp" line="150"/>
         <source>Save as...</source>
         <translation>Lagra som...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="173"/>
+
+        <location filename="../mainwindow.cpp" line="158"/>
         <source>&amp;Close</source>
         <translation>&amp;Stäng</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="181"/>
+
+        <location filename="../mainwindow.cpp" line="168"/>
         <source>&amp;Print</source>
         <translation>&amp;Skriv ut</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="189"/>
+
+        <location filename="../mainwindow.cpp" line="178"/>
         <source>&amp;Exit</source>
         <translation>&amp;Avsluta</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="195"/>
+
+        <location filename="../mainwindow.cpp" line="186"/>
         <source>&amp;Edit</source>
         <translation>&amp;Redigera</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="197"/>
+
+        <location filename="../mainwindow.cpp" line="188"/>
         <source>&amp;Undo</source>
         <translation>&amp;Ångra</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="203"/>
+
+        <location filename="../mainwindow.cpp" line="196"/>
         <source>&amp;Redo</source>
         <translation>&amp;Gör om</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="211"/>
+
+        <location filename="../mainwindow.cpp" line="206"/>
         <source>&amp;Copy</source>
         <translation>&amp;Kopiera</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="218"/>
+
+        <location filename="../mainwindow.cpp" line="214"/>
         <source>&amp;Paste</source>
         <translation>&amp;Klistra in</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="224"/>
+
+        <location filename="../mainwindow.cpp" line="222"/>
         <source>C&amp;ut</source>
         <translation>&amp;Klipp ut</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="233"/>
+
+        <location filename="../mainwindow.cpp" line="232"/>
         <source>&amp;Settings</source>
         <translation>&amp;Inställningar</translation>
     </message>
@@ -179,21 +225,48 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="270"/>
+
+        <source>Cursor</source>
+        <translation>Kursor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>Lastic</source>
+        <translation>Senaste</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="252"/>
+        <source>Pipette</source>
+        <translation>Pipett</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="257"/>
+        <source>Loupe</source>
+        <translation>Lupp</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="262"/>
         <source>Pen</source>
         <translation>Penna</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="277"/>
+
+        <location filename="../mainwindow.cpp" line="267"/>
         <source>Line</source>
         <translation>Linje</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="284"/>
+
+        <location filename="../mainwindow.cpp" line="272"/>
         <source>Spray</source>
         <translation>Spray</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="291"/>
+
+        <location filename="../mainwindow.cpp" line="277"/>
         <source>Fill</source>
         <translation>Fyllning</translation>
     </message>
@@ -204,6 +277,13 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="305"/>
+
+        <location filename="../mainwindow.cpp" line="282"/>
+        <source>Rect</source>
+        <translation>Rektangel</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="287"/>
         <source>Ellipse</source>
         <translation>Ellips</translation>
     </message>
@@ -219,16 +299,22 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="327"/>
+
+        <location filename="../mainwindow.cpp" line="292"/>
         <source>E&amp;ffects</source>
         <translation>E&amp;ffekter</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="329"/>
+
+        <location filename="../mainwindow.cpp" line="294"/>
         <source>Gray</source>
         <translation>Grå</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="334"/>
+
+        <location filename="../mainwindow.cpp" line="299"/>
         <source>Negative</source>
         <translation>Negativ</translation>
     </message>
@@ -259,6 +345,8 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="364"/>
+
+        <location filename="../mainwindow.cpp" line="304"/>
         <source>&amp;Tools</source>
         <translation>&amp;Verktyg</translation>
     </message>
@@ -274,6 +362,18 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="374"/>
+
+        <location filename="../mainwindow.cpp" line="306"/>
+        <source>Image size</source>
+        <translation>Bildstorlek</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="311"/>
+        <source>Canvas size</source>
+        <translation>Duk storlek</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="316"/>
         <source>Rotate</source>
         <translation>Rotera</translation>
     </message>
@@ -309,16 +409,32 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="411"/>
+
+        <location filename="../mainwindow.cpp" line="318"/>
+        <source>Left</source>
+        <translation>Vänster</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="325"/>
+        <source>Right</source>
+        <translation>Höger</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="334"/>
         <source>&amp;About</source>
         <translation>&amp;Om</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="413"/>
+
+        <location filename="../mainwindow.cpp" line="336"/>
         <source>&amp;About EasyPaint</source>
         <translation>&amp;Om EasyPaint</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="420"/>
+
+        <location filename="../mainwindow.cpp" line="344"/>
         <source>About Qt</source>
         <translation>Om Qt</translation>
     </message>
@@ -335,12 +451,18 @@
     <message>
         <location filename="../mainwindow.cpp" line="674"/>
         <location filename="../mainwindow.cpp" line="728"/>
+
+        <location filename="../mainwindow.cpp" line="491"/>
+        <location filename="../mainwindow.cpp" line="539"/>
         <source>Closing Tab...</source>
         <translation>Stängning tab...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="672"/>
         <location filename="../mainwindow.cpp" line="729"/>
+
+        <location filename="../mainwindow.cpp" line="492"/>
+        <location filename="../mainwindow.cpp" line="540"/>
         <source>File has been modified.
 Do you want to save changes?</source>
         <translation>Filen har ändrats.
@@ -348,21 +470,34 @@ Vill du spara ändringarna?</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="849"/>
+
+        <location filename="../mainwindow.cpp" line="820"/>
         <source>About EasyPaint</source>
         <translation>Om EasyPaint</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="860"/>
+
+        <location filename="../mainwindow.cpp" line="830"/>
         <source>version</source>
         <translation>version</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="860"/>
+
+        <location filename="../mainwindow.cpp" line="830"/>
         <source>Site</source>
         <translation>Site</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="860"/>
+
+        <location filename="../mainwindow.cpp" line="830"/>
+        <source>Copyright</source>
+        <translation>Copyright</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="830"/>
         <source>Authors</source>
         <translation>Författare</translation>
     </message>
@@ -378,58 +513,82 @@ Vill du spara ändringarna?</translation>
         <location filename="../widgets/palettebar.cpp" line="30"/>
         <source>Colors</source>
         <translation></translation>
+
+        <source>0.0.1</source>
+        <translation type="obsolete">0.0.1</translation>
     </message>
 </context>
 <context>
     <name>ResizeDialog</name>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="44"/>
+
+        <location filename="../resizedialog.cpp" line="42"/>
         <source>Resize</source>
         <translation>Ändra storlek</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="49"/>
+
+        <location filename="../resizedialog.cpp" line="47"/>
         <source>Original size:</source>
         <translation>Ursprunglig storlek:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="51"/>
+
+        <location filename="../resizedialog.cpp" line="49"/>
         <source>New size:</source>
         <translation>Ny storlek:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="54"/>
+
+        <location filename="../resizedialog.cpp" line="52"/>
         <source>Pixels</source>
         <translation>Pixlar</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="68"/>
         <location filename="../dialogs/resizedialog.cpp" line="86"/>
+
+        <location filename="../resizedialog.cpp" line="66"/>
+        <location filename="../resizedialog.cpp" line="84"/>
         <source>Width:</source>
         <translation>Bredd:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="69"/>
+
+        <location filename="../resizedialog.cpp" line="67"/>
         <source>x Height:</source>
         <translation>x Höjd:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="71"/>
+
+        <location filename="../resizedialog.cpp" line="69"/>
         <source>Percent</source>
         <translation>Procent</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="87"/>
+
+        <location filename="../resizedialog.cpp" line="85"/>
         <source>% x Height:</source>
         <translation>% x Höjd:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="88"/>
+
+        <location filename="../resizedialog.cpp" line="86"/>
         <source>%</source>
         <translation>%</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="90"/>
+
+        <location filename="../resizedialog.cpp" line="88"/>
         <source>Preserve Aspect Ratio</source>
         <translation>Bevara bildförhållande</translation>
     </message>
@@ -438,16 +597,22 @@ Vill du spara ändringarna?</translation>
     <name>SettingsDialog</name>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="49"/>
+
+        <location filename="../settingsdialog.cpp" line="45"/>
         <source>Settings</source>
         <translation>Inställningar</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="69"/>
+
+        <location filename="../settingsdialog.cpp" line="65"/>
         <source>Language:</source>
         <translation>Språk:</translation>
     </message>
     <message>
         <location filename="../dialogsettingsdialog.cpp" line="71"/>
+
+        <location filename="../settingsdialog.cpp" line="67"/>
         <source>&lt;System&gt;</source>
         <translation>&lt;System&gt;</translation>
     </message>
@@ -468,6 +633,8 @@ Vill du spara ändringarna?</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="91"/>
+
+        <location filename="../settingsdialog.cpp" line="76"/>
         <source>User interface</source>
         <translation>Användargränssnitt</translation>
     </message>
@@ -478,11 +645,20 @@ Vill du spara ändringarna?</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="107"/>
+
+        <location filename="../settingsdialog.cpp" line="79"/>
+        <source>Base size:</source>
+        <translation>Basstorlek</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="93"/>
         <source>History depth:</source>
         <translation>Historia djup:</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="113"/>
+
+        <location filename="../settingsdialog.cpp" line="101"/>
         <source>Autosave</source>
         <translation>Autolagra</translation>
     </message>
@@ -493,6 +669,13 @@ Vill du spara ändringarna?</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="130"/>
+
+        <location filename="../settingsdialog.cpp" line="103"/>
+        <source>Interval (msec):</source>
+        <translation>Interval (ms):</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="117"/>
         <source>Image</source>
         <translation>Bild</translation>
     </message>
@@ -565,12 +748,18 @@ Vill du spara ändringarna?</translation>
         <location filename="../dialogs/textdialog.cpp" line="100"/>
         <source>Clear text?</source>
         <translation></translation>
+
+        <location filename="../settingsdialog.cpp" line="127"/>
+        <source>Main</source>
+        <translation>Main</translation>
     </message>
 </context>
 <context>
     <name>ToolBar</name>
     <message>
         <location filename="../widgets/toolbar.cpp" line="37"/>
+
+        <location filename="../toolbar.cpp" line="35"/>
         <source>Instruments</source>
         <translation>Instrument</translation>
     </message>
@@ -583,14 +772,25 @@ Vill du spara ändringarna?</translation>
     <message>
         <location filename="../widgets/toolbar.cpp" line="93"/>
         <location filename="../widgets/toolbar.cpp" line="94"/>
+
+        <location filename="../toolbar.cpp" line="102"/>
+        <source>First color</source>
+        <translation>Första färg</translation>
+    </message>
+    <message>
+        <location filename="../toolbar.cpp" line="106"/>
         <source>Second color</source>
         <translation>Andra färg</translation>
     </message>
     <message>
         <location filename="../widgets/toolbar.cpp" line="100"/>
         <location filename="../widgets/toolbar.cpp" line="101"/>
+
+        <location filename="../toolbar.cpp" line="112"/>
         <source>Pen size</source>
         <translation>Penna storlek</translation>
     </message>
 </context>
+</TS>
+
 </TS>
