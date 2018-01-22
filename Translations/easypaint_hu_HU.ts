90ccf32b4f50ce886740ba182bb6d4c1dd460ad8
Tae-Woong Se <seotaewong40@gmail.com>
2018-01-22 21:07:55 +0900

    Added EasyPaint translations for three languages.

--- Translations/easypaint_hu_HU.ts
+++ Translations/easypaint_hu_HU.ts
@@ -1,3 +1,4 @@
+﻿
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="hu_HU">
@@ -31,6 +32,8 @@
     <name>ImageArea</name>
     <message>
         <location filename="../imagearea.cpp" line="178"/>
+        <location filename="../imagearea.cpp" line="107"/>
+
         <source>Open image...</source>
         <translation>Megnyitása kép...</translation>
     </message>
@@ -58,6 +61,9 @@
     </message>
     <message>
         <location filename="../imagearea.cpp" line="242"/>
+
+        <location filename="../imagearea.cpp" line="155"/>
+
         <source>Untitled image</source>
         <translation>Névtelen kép</translation>
     </message>
@@ -66,6 +72,17 @@
         <source>Save image...</source>
         <translation>Mentése kep...</translation>
     </message>
+
+        <location filename="../imagearea.cpp" line="157"/>
+        <source>Save image...</source>
+        <translation>Mentése kep...</translation>
+    </message>
+    <message>
+        <location filename="../imagearea.cpp" line="158"/>
+        <source>*.png;;*.jpg;;*.jpeg;;*.bmp;;*.xbm;;*.xpm</source>
+        <translation>*.png;;*.jpg;;*.jpeg;;*.bmp;;*.xbm;;*.xpm</translation>
+    </message>
+
 </context>
 <context>
     <name>MainWindow</name>
@@ -74,26 +91,42 @@
         <location filename="../mainwindow.cpp" line="489"/>
         <location filename="../mainwindow.cpp" line="537"/>
         <location filename="../mainwindow.cpp" line="544"/>
+
+        <location filename="../mainwindow.cpp" line="83"/>
+        <location filename="../mainwindow.cpp" line="398"/>
+
         <source>Untitled Image</source>
         <translation>Névtelen kép</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="147"/>
+
+        <location filename="../mainwindow.cpp" line="124"/>
+
         <source>&amp;File</source>
         <translation>&amp;Fájl</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="149"/>
+
+        <location filename="../mainwindow.cpp" line="126"/>
+
         <source>&amp;New</source>
         <translation>&amp;Új</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="155"/>
+
+        <location filename="../mainwindow.cpp" line="134"/>
+
         <source>&amp;Open</source>
         <translation>&amp;Megnyitása</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="161"/>
+
+        <location filename="../mainwindow.cpp" line="142"/>
+
         <source>&amp;Save</source>
         <translation>&amp;Mentése</translation>
     </message>
@@ -104,51 +137,86 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="173"/>
+
+        <location filename="../mainwindow.cpp" line="150"/>
+        <source>Save as...</source>
+        <translation>&amp;Mentése met...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="158"/>
+
         <source>&amp;Close</source>
         <translation>&amp;Bezárása</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="189"/>
+
+        <location filename="../mainwindow.cpp" line="168"/>
+
         <source>&amp;Print</source>
         <translation>&amp;Nyomtatás</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="195"/>
+
+        <location filename="../mainwindow.cpp" line="178"/>
+
         <source>&amp;Exit</source>
         <translation>&amp;Kilépés</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="197"/>
+
+        <location filename="../mainwindow.cpp" line="186"/>
+
         <source>&amp;Edit</source>
         <translation>&amp;Szerkesztés</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="203"/>
+
+        <location filename="../mainwindow.cpp" line="188"/>
+
         <source>&amp;Undo</source>
         <translation>&amp;Visszavonás</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="203"/>
+
+        <location filename="../mainwindow.cpp" line="196"/>
+
         <source>&amp;Redo</source>
         <translation>&amp;Előre</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="211"/>
+
+        <location filename="../mainwindow.cpp" line="206"/>
+
         <source>&amp;Copy</source>
         <translation>&amp;Másolás</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="218"/>
+
+        <location filename="../mainwindow.cpp" line="214"/>
+
         <source>&amp;Paste</source>
         <translation>&amp;Beillesztés</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="224"/>
+
+        <location filename="../mainwindow.cpp" line="222"/>
+
         <source>C&amp;ut</source>
         <translation>&amp;Kivágás</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="233"/>
+
+        <location filename="../mainwindow.cpp" line="232"/>
+
         <source>&amp;Settings</source>
         <translation>&amp;Beállítások</translation>
     </message>
@@ -179,21 +247,52 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="270"/>
+
+        <source>Cursor</source>
+        <translation>Kurzor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>Lastic</source>
+        <translation>Lastic</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="252"/>
+        <source>Pipette</source>
+        <translation>Pipetta</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="257"/>
+        <source>Loupe</source>
+        <translation>Loupa</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="262"/>
+
         <source>Pen</source>
         <translation>Toll</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="277"/>
+
+        <location filename="../mainwindow.cpp" line="267"/>
+
         <source>Line</source>
         <translation>Vonal</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="284"/>
+
+        <location filename="../mainwindow.cpp" line="272"/>
+
         <source>Spray</source>
         <translation>Permetező</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="291"/>
+
+        <location filename="../mainwindow.cpp" line="277"/>
+
         <source>Fill</source>
         <translation>Tölt</translation>
     </message>
@@ -204,6 +303,14 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="305"/>
+
+        <location filename="../mainwindow.cpp" line="282"/>
+        <source>Rect</source>
+        <translation>Téglalap</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="287"/>
+
         <source>Ellipse</source>
         <translation>Ellipszis</translation>
     </message>
@@ -219,16 +326,25 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="327"/>
+
+        <location filename="../mainwindow.cpp" line="292"/>
+
         <source>E&amp;ffects</source>
         <translation>E&amp;ffekt</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="329"/>
+
+        <location filename="../mainwindow.cpp" line="294"/>
+
         <source>Gray</source>
         <translation>Zöld</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="334"/>
+
+        <location filename="../mainwindow.cpp" line="299"/>
+
         <source>Negative</source>
         <translation>Negatív</translation>
     </message>
@@ -259,6 +375,9 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="364"/>
+
+        <location filename="../mainwindow.cpp" line="304"/>
+
         <source>&amp;Tools</source>
         <translation>&amp;Eszközök</translation>
     </message>
@@ -299,16 +418,50 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="411"/>
+
+        <location filename="../mainwindow.cpp" line="306"/>
+        <source>Image size</source>
+        <translation>Kép méret</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="311"/>
+        <source>Canvas size</source>
+        <translation>Kanavász méret</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="316"/>
+        <source>Rotate</source>
+        <translation>Forgatás</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="318"/>
+        <source>Left</source>
+        <translation>Jobbra</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="325"/>
+        <source>Right</source>
+        <translation>Balra</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="334"/>
+
         <source>&amp;About</source>
         <translation>&amp;Információ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="413"/>
+
+        <location filename="../mainwindow.cpp" line="336"/>
+
         <source>&amp;About EasyPaint</source>
         <translation>EasyPaint &amp;információ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="240"/>
+
+        <location filename="../mainwindow.cpp" line="344"/>
+
         <source>About Qt</source>
         <translation>Qt információ</translation>
     </message>
@@ -325,12 +478,20 @@
     <message>
         <location filename="../mainwindow.cpp" line="674"/>
         <location filename="../mainwindow.cpp" line="728"/>
+
+        <location filename="../mainwindow.cpp" line="491"/>
+        <location filename="../mainwindow.cpp" line="539"/>
+
         <source>Closing Tab...</source>
         <translation>Befejező fül...</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="672"/>
         <location filename="../mainwindow.cpp" line="729"/>
+
+        <location filename="../mainwindow.cpp" line="492"/>
+        <location filename="../mainwindow.cpp" line="540"/>
+
         <source>File has been modified.
 Do you want to save changes?</source>
         <translation>Fájl modósult.
@@ -338,21 +499,38 @@ Szeretné menteni a változásokat?</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="849"/>
+
+        <location filename="../mainwindow.cpp" line="820"/>
+
         <source>About EasyPaint</source>
         <translation>EasyPaint információ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="860"/>
+
+        <location filename="../mainwindow.cpp" line="830"/>
+
         <source>version</source>
         <translation>verzió</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="860"/>
+
+        <location filename="../mainwindow.cpp" line="830"/>
+
         <source>Site</source>
         <translation>Telek</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="860"/>
+
+        <location filename="../mainwindow.cpp" line="830"/>
+        <source>Copyright</source>
+        <translation>Copyright</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="830"/>
+
         <source>Authors</source>
         <translation>Szerző</translation>
     </message>
@@ -368,58 +546,93 @@ Szeretné menteni a változásokat?</translation>
         <location filename="../widgets/palettebar.cpp" line="30"/>
         <source>Colors</source>
         <translation>Szín</translation>
+
+        <source>0.0.1</source>
+        <translation type="obsolete">0.0.1</translation>
+
     </message>
 </context>
 <context>
     <name>ResizeDialog</name>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="44"/>
+
+        <location filename="../resizedialog.cpp" line="42"/>
+
         <source>Resize</source>
         <translation>Átméretez</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="49"/>
+
+        <location filename="../resizedialog.cpp" line="47"/>
+
         <source>Original size:</source>
         <translation>Között méret:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="51"/>
+
+        <location filename="../resizedialog.cpp" line="49"/>
+
         <source>New size:</source>
         <translation>Új méret:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="54"/>
+
+        <location filename="../resizedialog.cpp" line="52"/>
+
         <source>Pixels</source>
         <translation>Pixels</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="68"/>
         <location filename="../dialogs/resizedialog.cpp" line="86"/>
+
+        <location filename="../resizedialog.cpp" line="66"/>
+        <location filename="../resizedialog.cpp" line="84"/>
+
         <source>Width:</source>
         <translation>Szélesség:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="69"/>
+
+        <location filename="../resizedialog.cpp" line="67"/>
+
         <source>x Height:</source>
         <translation>x Magasság:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="71"/>
+
+        <location filename="../resizedialog.cpp" line="69"/>
+
         <source>Percent</source>
         <translation>Százalék</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="87"/>
+
+        <location filename="../resizedialog.cpp" line="85"/>
+
         <source>% x Height:</source>
         <translation>% x Magasság:</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="88"/>
+
+        <location filename="../resizedialog.cpp" line="86"/>
+
         <source>%</source>
         <translation>%</translation>
     </message>
     <message>
         <location filename="../dialogs/resizedialog.cpp" line="90"/>
+
+        <location filename="../resizedialog.cpp" line="88"/>
+
         <source>Preserve Aspect Ratio</source>
         <translation>Befőtt szemszög arányszám</translation>
     </message>
@@ -428,16 +641,25 @@ Szeretné menteni a változásokat?</translation>
     <name>SettingsDialog</name>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="49"/>
+
+        <location filename="../settingsdialog.cpp" line="45"/>
+
         <source>Settings</source>
         <translation>Beállítások</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="69"/>
+
+        <location filename="../settingsdialog.cpp" line="65"/>
+
         <source>Language:</source>
         <translation>Nyelv:</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="71"/>
+
+        <location filename="../settingsdialog.cpp" line="67"/>
+
         <source>&lt;System&gt;</source>
         <translation>&lt;Rendszer&gt;</translation>
     </message>
@@ -458,6 +680,9 @@ Szeretné menteni a változásokat?</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="91"/>
+
+        <location filename="../settingsdialog.cpp" line="76"/>
+
         <source>User interface</source>
         <translation>Használó elválasztó felület</translation>
     </message>
@@ -468,11 +693,22 @@ Szeretné menteni a változásokat?</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="107"/>
+
+        <location filename="../settingsdialog.cpp" line="79"/>
+        <source>Base size:</source>
+        <translation>Bázis méret:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="93"/>
+
         <source>History depth:</source>
         <translation>Történelem mélyseg:</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="113"/>
+
+        <location filename="../settingsdialog.cpp" line="101"/>
+
         <source>Autosave</source>
         <translation>Önműködő mentett</translation>
     </message>
@@ -483,6 +719,14 @@ Szeretné menteni a változásokat?</translation>
     </message>
     <message>
         <location filename="../dialogs/settingsdialog.cpp" line="130"/>
+
+        <location filename="../settingsdialog.cpp" line="103"/>
+        <source>Interval (msec):</source>
+        <translation>Távolság (ms):</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="117"/>
+
         <source>Image</source>
         <translation>Kép</translation>
     </message>
@@ -556,11 +800,20 @@ Szeretné menteni a változásokat?</translation>
         <source>Clear text?</source>
         <translation>Törlése szöveg?</translation>
     </message>
+
+        <location filename="../settingsdialog.cpp" line="127"/>
+        <source>Main</source>
+        <translation>Fő</translation>
+    </message>
+
 </context>
 <context>
     <name>ToolBar</name>
     <message>
         <location filename="../widgets/toolbar.cpp" line="37"/>
+
+        <location filename="../toolbar.cpp" line="35"/>
+
         <source>Instruments</source>
         <translation>Hangszerel</translation>
     </message>
@@ -573,6 +826,14 @@ Szeretné menteni a változásokat?</translation>
     <message>
         <location filename="../widgets/toolbar.cpp" line="93"/>
         <location filename="../widgets/toolbar.cpp" line="94"/>
+
+        <location filename="../toolbar.cpp" line="102"/>
+        <source>First color</source>
+        <translation>Első szín</translation>
+    </message>
+    <message>
+        <location filename="../toolbar.cpp" line="106"/>
+
         <source>Second color</source>
         <translation>Másod szín</translation>
     </message>
@@ -584,3 +845,4 @@ Szeretné menteni a változásokat?</translation>
     </message>
 </context>
 </TS>
+
