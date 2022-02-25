51023fa13f513352c6321026ff732b97b669e488
twse <seotaewong40@gmail.com>
2022-02-26 05:24:29 +0900

    Moved EasyPaint translations and translated Sobreloader to German

---
+++ Translations/Sobreloader in German/AcercaDe.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sobreloader
+{
+    public partial class AcercaDe : Form
+    {
+        public AcercaDe()
+        {
+            InitializeComponent();
+            Strings SC = new Strings();
+            label2.Text = "Version ";
+            label2.Text += SC.obtenerVersion();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/user/elstef41");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://twitter.com/elstef41");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://elstef41.cf/ (auf spanisch)");
+        }
+
+        private void AcercaDe_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 27)
+            {
+                this.Close();
+            }
+        }
+    }
+}
